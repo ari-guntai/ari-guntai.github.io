400d5f443e722cdbfaca733cb53b1319cdfe067e
ari-guntai <ankoromoti1304@gmail.com>
2026-07-06 13:41:24 +0900

    Add files via upload

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// 左上のロゴを変える
+export const SITE_TITLE = 'あすとろぶろぐ';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
---
+++ src/content/blog/astoro-howto.md
@@ -0,0 +1,8 @@
+---
+title: タイトル
+description: '説明'
+pubDate: '2026-07-03'
+tags: ["日記"]
+---
+
+# タイトル
